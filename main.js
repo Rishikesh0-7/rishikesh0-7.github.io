29da6f58262cee4567aa90e70b97142bb04a4578
Rishikesh <68162560+Rishikesh0-7@users.noreply.github.com>
2021-09-03 19:00:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const typed = new Typed("#typing-stuff", {
+    strings: ["Programmer", "Python Developer ", "Discord Bot Developer","Tech Savvy" , "Web Developer"],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 40,
+});
+
+
+particlesJS.load('particles-js', 'particlesjs-config.json', () => {
+    console.log('Particles.js config loaded!');
+});
+
+
+
+function gotoa() {
+    document.getElementById('about-me').scrollIntoView(true);
+}
+function gotos() {
+    document.getElementById('skills').scrollIntoView(true);
+}
+function gotop() {
+    document.getElementById('projects').scrollIntoView(true);
+}
+function bars(){
+    document.getElementById("navbar").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("navbar").style.width = "0%";
+}
